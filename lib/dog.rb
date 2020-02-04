1a44051f922496cfe03274c39dc6c900cc0c7bcc
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-02-04 02:47:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,12 @@
 class Dog 
   
   def name=(dogs_name)
-    this_dogs_name = dogs_name
+    @this_dogs_name = dogs_name
   end
+ 
+  def name
+    @this_dogs_name
+  end
+
   
   end  # Your code goes here!
